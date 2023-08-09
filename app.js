675aac20cd5e805218c9da335f55b92f5610fbfd
satish <satishsuryawanshi680@gmail.com>
2023-08-09 12:57:01 +0530

    change

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 
-app.get("/", (req, res) => res.send("Hello world"));
+app.get("/", (req, res) => res.send("Hello"));
 app.get("/health", (req, res) => {
   res.status(200);
   res.send("healthy");
